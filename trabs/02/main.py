013675b77b0c4e5933d43bb36b3aa75e59cc5dac
ArtV97 <arthurvianna@id.uff.br>
2022-05-16 21:24:09 -0300

    trab 02 old python file deleted

--- trabs/02/main.py
+++ trabs/02/main.py
@@ -1,85 +1,84 @@
-# rota de serviço: é composta por C cidades e C−1 estradas: a primeira cidade da rota de serviço é a origem da encomenda,
-## a última o destino da encomenda. A rota de serviço não passa duas vezes pela mesma cidade, e o veículo escolhido para 
-## fazer o transporte de uma encomenda pode trafegar apenas pela rota de serviço definida.
+import heapq
 
-#representando, respectivamente, o número de cidades do país, o número de estradas, o número de cidades na rota de serviço e a 
-#cidade em que o veículo foi consertado. As cidades são identificadas por inteiros de 0 a N−1. A rota de serviço é 0, 1, ... , 
-#C−1, ou seja, a origem é 0, de 0 passa para 1, de 1 para 2 e assim por diante, até o destino C−1.
-
-
-def bfs(grafo, root, t, L, pai, C, F = []):
-    t[0] += 1
-    L[root] = t[0]
-
-    F.append(root)
+def alterar_prioridade(w,D):
+    for i in range(len(D)):
+        if D[i][1] == w:
+            pos = i
+            break
+    D[pos] = (L[w], w)
+    heapq._siftdown(D, 0, pos)
+    return
 
 
-    while len(F) > 0:
-        v = F.pop()
 
-        if grafo[v] is None: continue # don't have neighbours
+while True:
+    N, M, C, K = map(int, input().split())
+
+    if N == 0 and M == 0 and C == 0 and K == 0:
+        break
+
+    grafo = [[] * N for i in range(N)]  # definir listas de adjacencia
+    custo = []
+    infty = 99999999
+    # definir a matriz de custos (pesos)
+    for i in range(N):
+        linha = []
+        for j in range(N):
+            if i == j:
+                linha.append(0)
+            else:
+                linha.append(infty)
+        custo.append(linha)
+
+    for j in range(M):
+        # estrada entre U e V com pedagio P
+        U, V, P = map(int, input().split())
+
+        grafo[U].append(V)
+        grafo[V].append(U)
+        custo[U][V] = P
+        custo[V][U] = P
+
+    #inicializacoes
+
+    marca = N*[0]
+    L = N*[infty]
+    raiz = K
+    L[raiz] = 0
+    D = [(0, raiz)]      # cada posição do heap guarda (L(w), w)
+    for w in range(0, N):
+        if w != raiz:
+            heapq.heappush(D, (L[w], w))  # iniciar o heap com todos os valores
+    pai = N*[-1]
+
+    # print(f"N = {N}, M = {M}, C = {C}, K = {K}")
+
+    while D != []:
+        Lmin, v = heapq.heappop(D)      # tirar a raiz do heap
+        marca[v] = 1
 
         # restricao do problema
-        # if v < C:
-        #     prev = v
-        #     for rota in range(v+1, C):
-        #         print(v, rota, pai)
-        #         t[0] += 1
-        #         L[rota] = t[0]
-        #         pai[rota] = prev
-        #         prev = rota
+        if v < C:
+            custo_v2x = L[v]
+            for x in range(v, C-1):
+                custo_v2x += custo[x][x+1]
+                if custo_v2x < L[x+1]:
+                    L[x+1] = custo_v2x
             
-        #     return
+            continue
 
+        for w in grafo[v]:
+            if marca[w] == 0:
+                custo_v2w = L[v]
+                
+                custo_v2w += custo[v][w]
+                    
+                if custo_v2w < L[w]:
+                    L[w] = custo_v2w          # atualizar o valor de L[w]
+                    alterar_prioridade(w, D)    # atualizar o heap
+                    pai[w] = v
 
-        for w in range(len(grafo)):
-            if grafo[v][w] is None: continue # isn't neighbour
-
-            if L[w] == 0:
-                pai[w] = v
-                t[0] += 1
-                L[w] = t[0]
-                F.append(w)
-
-
-
-# N cidades
-# M estradas
-# C cidades na rota de servico. A rota de serviço é 0, 1, ... , C−1
-# K cidade na qual o veiculo foi consertado
-if __name__ == "__main__":
-    while True:
-        N, M, C, K = map(int, input().split())
-
-        if N == 0 and M == 0 and C == 0 and K == 0:
-            break
-
-        grafo = [None for j in range(N)]
-        for i in range(M):
-            # estrada entre U e V com pedagio P
-            U, V, P = map(int, input().split())
-
-            if grafo[U] is None:
-                grafo[U] = [None for i in range(N)]
-            if grafo[V] is None:
-                grafo[V] = [None for i in range(N)]
-            
-            grafo[U][V] = P
-            grafo[V][U] = P
-
-        L = [0 for x in range(N)]
-        pai = [None for x in range(N)]
-
-        t = [0]
-        bfs(grafo, K, t, L, pai, C)
-        print(f"N = {N}, M = {M}, C = {C}, K = {K}")
-        print(f"L: {L}")
-        print(f"pai: {pai}")
-
-        cost = 0
-        v = C-1
-        while pai[v] is not None:
-            cost += grafo[pai[v]][v]
-            v = pai[v]
-
-        print(f"Cost = {cost}\n")
+    # print(f"L = {L}")
+    # print(f"pai = {pai}")
+    # print("\n\n")
+    print(L[C-1])
